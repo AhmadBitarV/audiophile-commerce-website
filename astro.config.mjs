f76f6569d24ed1b0980cc013cf7bd3d0be5aa2b5
Ahmad <ahmad.bitar@nordsec.com>
2024-10-26 23:49:20 +0300

    feat: connect audiophile to storyblok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,26 @@
 import { defineConfig } from "astro/config";
+import storyblok from "@storyblok/astro";
+import { loadEnv } from "vite";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
-import tailwind from "@astrojs/tailwind";
+const env = loadEnv("", process.cwd(), "STORYBLOK");
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        page: "storyblok/Page",
+      },
+      apiOptions: {
+        region: "eu",
+      },
+    }),
+  ],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
 });
