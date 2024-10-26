84d744e83091afc260b55d25290f6fe0cf2f692e
Ahmad <ahmad.bitar@nordsec.com>
2024-10-27 00:03:20 +0300

    feat: install solid js

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,20 @@ import storyblok from "@storyblok/astro";
 import { loadEnv } from "vite";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 
+import solidJs from "@astrojs/solid-js";
+
 const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 export default defineConfig({
-  integrations: [
-    storyblok({
-      accessToken: env.STORYBLOK_TOKEN,
-      components: {
-        page: "storyblok/Page",
-      },
-      apiOptions: {
-        region: "eu",
-      },
-    }),
-  ],
+  integrations: [storyblok({
+    accessToken: env.STORYBLOK_TOKEN,
+    components: {
+      page: "storyblok/Page",
+    },
+    apiOptions: {
+      region: "eu",
+    },
+  }), solidJs()],
   vite: {
     plugins: [basicSsl()],
     server: {
