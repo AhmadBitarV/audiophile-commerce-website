aa45566974582f19ef9c2f76334d676ebb961410
Ahmad <ahmad.bitar@nordsec.com>
2024-11-12 00:49:57 +0200

    fix: deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,36 +1,26 @@
 import { defineConfig } from "astro/config";
-
-import tailwind from "@astrojs/tailwind";
-
-import solidJs from "@astrojs/solid-js";
 import storyblok from "@storyblok/astro";
 import { loadEnv } from "vite";
+import tailwind from "@astrojs/tailwind";
 import basicSsl from "@vitejs/plugin-basic-ssl";
-import vercel from "@astrojs/vercel/serverless";
-
 const env = loadEnv("", process.cwd(), "STORYBLOK");
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [
     storyblok({
       accessToken: env.STORYBLOK_TOKEN,
-      bridge: env.STORYBLOK_IS_PREVIEW === "yes",
       components: {
         page: "storyblok/Page",
         grid: "storyblok/Grid",
       },
     }),
     tailwind(),
-    solidJs(),
   ],
-  output: env.STORYBLOK_IS_PREVIEW === "yes" ? "server" : "static",
-  ...(env.STORYBLOK_ENV === "development" && {
-    vite: {
-      plugins: [basicSsl()],
-      server: {
-        https: true,
-      },
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
     },
-  }),
-  adapter: vercel(),
+  },
 });
