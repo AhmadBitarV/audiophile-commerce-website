b3bb98c693c4dc73d2abf33b628338396b26e61e
Ahmad <ahmad.bitar@nordsec.com>
2024-11-20 12:54:53 +0200

    fix: preview and production deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import basicSsl from "@vitejs/plugin-basic-ssl";
 import vercel from "@astrojs/vercel/serverless";
 const env = loadEnv("", process.cwd(), "STORYBLOK");
 
+console.log(env);
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
